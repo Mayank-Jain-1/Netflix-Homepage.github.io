22c4844d8adb55e1451b922d94d5cfb2dd60f19b
Mayank <kaushalmayank.k@gmail.com>
2022-12-23 01:50:54 +0530

    faq section complete

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const displayAnswer = (e) => {
-  
-  console.log(e)
+  const question = document.getElementById(`question${e}`);
+  const caretDown = question.getElementsByClassName('zmdi-caret-down')[0];
+  const caretUp = question.getElementsByClassName('zmdi-caret-up')[0];
+  caretDown.classList.toggle('noDisplay')
+  caretUp.classList.toggle('noDisplay')
+  const answer = document.getElementById(`answer${e}`)
+  answer.classList.toggle('noDisplay')
 }
